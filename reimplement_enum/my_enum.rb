8d76186948f95439f0a1bf3e60967536399578d9
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-07-31 08:12:49 +0900

    enumを実装

--- reimplement_enum/my_enum.rb
+++ reimplement_enum/my_enum.rb
@@ -5,6 +5,51 @@ module MyEnum
 
   module ClassMethods
     def enum(definitions)
+      definitions.keys.each do |column_name|
+        statuses =
+          case definitions[column_name]
+          when Array
+            definitions[column_name].each_with_object({}).with_index { |(status_name, hash), status_num| hash[status_name] = status_num }
+          when Hash
+            definitions[column_name]
+          end
+
+        define_method(column_name) do
+          statuses.find { |status_name, status_num| instance_variable_get("@#{column_name}") == status_num }.first
+        end
+
+        status_names = statuses.keys
+
+        status_names.each do |status_name|
+          define_method("#{status_name}?") do
+            instance_variable_get("@#{column_name}") == statuses[status_name]
+          end
+
+          define_method("#{status_name}!") do
+            instance_variable_set("@#{column_name}", statuses[status_name])
+          end
+
+          define_singleton_method(status_name) do
+            $database.select { |record| record.instance_variable_get("@#{column_name}") == statuses[status_name] }
+          end
+
+          define_singleton_method("not_#{status_name}") do
+            $database.reject { |record| record.instance_variable_get("@#{column_name}") == statuses[status_name] }
+          end
+        end
+
+        status_name_pairs = status_names.each_with_object({}) do |status_name, hash|
+          hash[status_name] = status_names.dup.tap { |arr| arr.delete(status_name) }
+        end
+
+        status_name_pairs.each do |key, vals|
+          vals.each do |val|
+            define_singleton_method("#{key}_or_#{val}") do
+              send(key) | send(val)
+            end
+          end
+        end
+      end
     end
   end
 end
